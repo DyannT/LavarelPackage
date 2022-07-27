f868aa78625c7d2ec0c421c4513faa421fbf8caa
unknown <lopk1162002@gmail.com>
2022-07-27 23:58:27 +0700

    packageL

---
+++ core/demo/Providers/DemoServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Botble\Demo\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class DemoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
